4f713fa2643e45455976f0be1c4e8684563b6ea7
abdenour <bareche.abdenour@gmail.com>
2019-09-23 18:58:06 +0200

    Updates

---
+++ precache-manifest.ae052687a972486573885cd616c24420.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0018398b0e45a630c84f546ddd9616aa",
+    "url": "/mynaur/boutique.github.io.git/index.html"
+  },
+  {
+    "revision": "d67f667f4653d750ffa6",
+    "url": "/mynaur/boutique.github.io.git/static/css/main.cfa4ae76.chunk.css"
+  },
+  {
+    "revision": "3f7e86d329631af082bc",
+    "url": "/mynaur/boutique.github.io.git/static/js/2.7c69a712.chunk.js"
+  },
+  {
+    "revision": "d67f667f4653d750ffa6",
+    "url": "/mynaur/boutique.github.io.git/static/js/main.313ad8d0.chunk.js"
+  },
+  {
+    "revision": "73bad25a8179d34e237a",
+    "url": "/mynaur/boutique.github.io.git/static/js/runtime~main.db9f3942.js"
+  },
+  {
+    "revision": "00b9bb9d8170f95c4159499f086e8c15",
+    "url": "/mynaur/boutique.github.io.git/static/media/App.00b9bb9d.less"
+  },
+  {
+    "revision": "66095b0798788236f877e7b1faf6d6c1",
+    "url": "/mynaur/boutique.github.io.git/static/media/mix-background.66095b07.png"
+  }
+]);
